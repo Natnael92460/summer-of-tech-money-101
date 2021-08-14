ad739ce8a7313cb6fcd3e0fa01183fc5c04c449f
Meng <dreamsjtu@gmail.com>
2021-08-15 10:11:12 +1200

    layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,11 @@
-import { Box, HStack } from "@chakra-ui/react"
-
+import { Box, HStack, Badge, Image, StarIcon} from "@chakra-ui/react"
+import {AirbnbExample} from "../components/AirbnbExample"
 
 export const Home = () => {
-  const boxHeight = 30;
-  return (
-    <div>
-      <h1>Home</h1>
-      <HStack spacing="24px">
-      <Box bg="tomato" w="100%" p={boxHeight} color="white">
-        This is the Box1
-      </Box>
-      <Box bg="green" w="100%" p={boxHeight} color="white">
-        This is the Box2
-      </Box>
-      <Box bg="blue" w="100%" p={boxHeight} color="white">
-        This is the Box3
-      </Box>
-      </HStack>
-    </div>
-  );
+return (
+  <div>
+    <span>Hello</span>
+    <AirbnbExample></AirbnbExample>
+  </div>
+);
 };
