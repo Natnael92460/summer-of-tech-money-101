76f4fa501359d77730b4538e83d14d0dd972863e
tanmichael1 <michaelrstan@gmail.com>
2021-08-15 11:52:13 +1200

    Put Trade Me items in grids, grid sections fill screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,57 @@
-import React, {useState, useEffect} from "react";
-import { Box, HStack, Badge, Image, StarIcon,Grid,GridItem} from "@chakra-ui/react"
-import {AirbnbExample} from "../components/AirbnbExample"
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  HStack,
+  Badge,
+  Image,
+  StarIcon,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
+import { AirbnbExample } from "../components/AirbnbExample";
 
-function siplifySampleData(allSearchResult){
+function siplifySampleData(allSearchResult) {
   return allSearchResult.List.map(function (allData) {
-    return {listId    : allData.ListingId, 
-            listTitle : allData.Title, 
-            listPrice : allData.BuyNowPrice,
-            listPic   : allData.PictureHref };
-  })
+    return {
+      listId: allData.ListingId,
+      listTitle: allData.Title,
+      listPrice: allData.BuyNowPrice,
+      listPic: allData.PictureHref,
+    };
+  });
 }
 
 export const Home = () => {
   const [searchResults, setSearchResults] = useState([]);
 
-    useEffect(async () => {
-      const response = await fetch('/Sample-Api.json').then(response => response.json());
-      
-      setSearchResults (siplifySampleData(response));
-    }, [])
-return (
-<Grid
-  h="200px"
-  templateRows="repeat(2, 1fr)"
-  templateColumns="repeat(2, 1fr)"
-  gap={4}
->
-  <GridItem bg="tomato" />
-  
-  <GridItem rowSpan={2} bg="papayawhip" >
-  {searchResults.map((searchResult) => 
-      (<div> 
-        <img src={searchResult.listPic}/>
-       <p> {searchResult.listTitle}</p>
-       <p>
-        ${searchResult.listPrice}</p>
-        </div>))}
-  </GridItem>
-  <GridItem bg="yellow" />  
-  
-  
-</Grid>
-);
+  useEffect(async () => {
+    const response = await fetch("/Sample-Api.json").then((response) =>
+      response.json()
+    );
+
+    setSearchResults(siplifySampleData(response));
+  }, []);
+  return (
+    <Grid
+      h="100vh"
+      templateRows="repeat(2, 1fr)"
+      templateColumns="repeat(2, 1fr)"
+      gap={4}
+    >
+      <GridItem bg="tomato" />
+
+      <GridItem rowSpan={2} style={{ overflow: "scroll" }} bg="papayawhip">
+        <Grid templateColumns="repeat(2, 1fr)">
+          {searchResults.map((searchResult) => (
+            <GridItem>
+              <img src={searchResult.listPic} />
+              <p> {searchResult.listTitle}</p>
+              <p>${searchResult.listPrice}</p>
+            </GridItem>
+          ))}
+        </Grid>
+      </GridItem>
+      <GridItem bg="yellow" />
+    </Grid>
+  );
 };
