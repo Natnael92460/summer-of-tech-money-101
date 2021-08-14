cb210eb1297f1aaa13a9a820f1c244bf7e60f285
tanmichael1 <michaelrstan@gmail.com>
2021-08-14 15:12:51 +1200

    Add react router, add 4 pages, change title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Goals } from "./pages/Goals";
+import { Progress } from "./pages/Progress";
+import { Page4 } from "./pages/page4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/goals">Goals</Link>
+              </li>
+              <li>
+                <Link to="/progress">Progress</Link>
+              </li>
+              <li>
+                <Link to="/page4">Page4</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/goals">
+              <Goals />
+            </Route>
+            <Route path="/progress">
+              <Progress />
+            </Route>
+            <Route path="/page4">
+              <Page4 />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Progress.jsx
@@ -0,0 +1,7 @@
+export const Progress = () => {
+  return (
+    <div>
+      <h1>Progress</h1>
+    </div>
+  );
+};
