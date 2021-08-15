23089b734b92c7d74287967088f01ea35c21e6d8
Meng <dreamsjtu@gmail.com>
2021-08-15 15:24:33 +1200

    Add titles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,6 +54,7 @@ export const Home = () => {
       gap={4}
     >
       <GridItem style={{marginLeft:"30px" , marginDown:"40px"}} bg="white">
+      <h1 style={{textAlign:"center",color:"#60BB77"}}>Ways to earn</h1>
       <Grid templateColumns="repeat(3, 1fr)">
         {chores.map((chore) => (
           <GridItem>
@@ -70,6 +71,7 @@ export const Home = () => {
 
 
       <GridItem rowSpan={2} style={{ overflow: "scroll" , marginLeft:"20px",marginDown:"40px" }} bg="white">
+      <h1 style={{textAlign:"center",color:"#60BB77"}}>Choose a Savings Goal</h1>
         <Grid templateColumns="repeat(2, 1fr)">
           {searchResults.map((searchResult) => (
             <GridItem >
@@ -82,7 +84,7 @@ export const Home = () => {
       </GridItem> 
 
       <GridItem bg="white">
-        <h1 style={{textAlign:"center"}}>My Progress</h1>
+        <h1 style={{textAlign:"center",color:"#60BB77"}}>My Progress</h1>
       <Grid
       h="60vh"
       templateRows="repeat(2, 1fr)"
